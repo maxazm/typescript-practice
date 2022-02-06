d8c2371f6a5626e92607f98fb1feacff61d10a3f
maxazm <zomk0222@gmail.com>
2022-02-06 18:12:18 +0900

    section4

--- course-starting-project/src/app.ts
+++ course-starting-project/src/app.ts
@@ -2,6 +2,23 @@
 // tsc --init (load all files in the project)
 // use "tsc" to update
 
-const userName2= "hello";
+const UserName = "max";
 
-console.log(userName2);
+let age = 30;
+
+age = 29;
+
+// default arguments
+const multiply = (n1: number, n2: number = 3) => {
+    return n1 * n2;
+};
+
+// shorter ver.
+const multiply_shorter = (n1: number, n2: number) => n1 * n2;
+
+console.log(multiply(3));
+console.log(multiply_shorter(4,5));
+
+const button = document.querySelector("button")!;
+
+button.addEventListener("click", e => console.log(e));
